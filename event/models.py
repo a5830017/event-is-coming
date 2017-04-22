5886ece5efee96ff337ca1964dbc8fce7192df96
a5830017 ubuntu <a5830017@gmail.com>
2017-04-22 23:23:57 +0700

    create home page

--- event/models.py
+++ event/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+#from django.utils import timezone
 
-# Create your models here.
+class Event(models.Model):
+    event_name = models.TextField(default='') #name
+    event_detail = models.TextField(default='') #detail
+    event_numset = models.TextField(default='') #limit for start
+    event_location = models.TextField(default='') #location
+    #date = models.DateTimeField(default=timezone.now())#'date published')YYYY-MM-DD HH:MM
+
+class Person(models.Model):
+    fname = models.TextField(default='') #first name
+    lname = models.TextField(default='') #last name
+    pcount = models.IntegerField(default=0) #person count
--- event/views.py
+++ event/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+
+from .models import Event , Person
 
 # Create your views here.
+def home(request):
+    event_list = Event.objects.order_by('event_name')
+    person_count = Person.objects.all()
+    context = {'event_list': event_list}
+    context2 = {'person': person_count}
+    return render(request, 'home.html', context, context2)
+
+
+def create_event(request):
+    if request.method == 'POST':
+        Event.objects.create(event_name=request.POST['name'], event_detail=request.POST['detail'],
+            event_numset=request.POST['numset'], event_location=request.POST['location'])
+        return redirect('/')
+
+    event = Event.objects.all()
+    return render(request, 'create_event.html', {'event': event})
